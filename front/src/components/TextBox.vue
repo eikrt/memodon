9146698504222bc230addfc8d8c8b9cf8c1e7bec
Eino Korte <einokorte@pop-os.localdomain>
2020-12-15 14:38:29 +0200

    added stuff

--- front/src/components/TextBox.vue
+++ front/src/components/TextBox.vue
@@ -1,16 +1,16 @@
 <template>
 
   <div>
-    
-    
-   <div> 
+    <div>
+      <h1>Input title and text and get it as org-mode usable</h1>
    <textarea v-model="inputtitle" placeholder="Write a title"></textarea>
    </div>
    <div>
    <textarea v-model="inputtext" placeholder="Write some text!"></textarea>
    </div>
    <div>
-  <button> Submit!</button>
+     <p>* {{inputtitle}}</p>
+     <p> {{inputtext}}</p>
 </div>
  </div>
 
