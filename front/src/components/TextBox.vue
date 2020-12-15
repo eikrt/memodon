977f394f6fcefcad0ab085ec2c177b39dfcafc7d
Eino Korte <einokorte@pop-os.localdomain>
2020-12-15 10:56:53 +0200

    initial commit

---
+++ front/src/components/TextBox.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div>
+    
+    
+   <div> 
+   <textarea v-model="inputtitle" placeholder="Write a title"></textarea>
+   </div>
+   <div>
+   <textarea v-model="inputtext" placeholder="Write some text!"></textarea>
+   </div>
+   <div>
+  <button> Submit!</button>
+</div>
+ </div>
+
+  
+</template>
+<script>
+  export default
+  {
+  props: {
+  
+  inputtitle: {required: true, type: String},
+  inputtext: {required: true, type: String}
+  }
+  }
+</script>
+
